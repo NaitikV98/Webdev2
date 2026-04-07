c3883a013a9fcb00ac7922eab1c8e21ee7464e7b
NaitikV98 <naitikvarshney1111@gmail.com>
2026-04-07 19:21:57 +0530

    exp

---
+++ dom.js
@@ -0,0 +1,7 @@
+  function result(){
+        let changeColor=document.getElementById("text");
+        changeColor.style.color="red";
+
+        let bodyColor=document.getElementByTagName("body")[0];
+        bodyColor.style.backgroundColor="yellow";
+    }
